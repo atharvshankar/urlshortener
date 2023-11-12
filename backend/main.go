a6999273a3a80e89f69ec19ede69f68249b66403
atharvs <shankaratharv@gmail.com>
2023-11-12 16:41:26 +0530

    Completed Essential parts of Backend APIs. Scaffolding frontend

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 	"strconv"
+	"time"
 	"database/sql"
 	"github.com/joho/godotenv"
 	"github.com/gofiber/fiber/v2"
@@ -38,6 +39,7 @@ func main() {
 		password string = os.Getenv("DB_PASSWORD")
 		dbname string = os.Getenv("DB_NAME")
 	)
+	const domainName string = "http://localhost:3000/"
 	
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
@@ -56,10 +58,18 @@ func main() {
 	fmt.Println("Connected to DB Successfully!")
 
 	var counter int64
-	counter = 100000
-
+	// counter = 100000
 	//Read next counter value from DB during startup. Helpful when backend crashes or is restarted.
-	//TODO: counter = ReadCounterValueFromDB()
+	// counter = ReadCounterValueFromDB()
+	row := db.QueryRow("SELECT value FROM tracker WHERE name = $1","counter")
+	switch err := row.Scan(&counter); err{
+	case sql.ErrNoRows:
+		//Stop backend if you cant fetch the counter value.
+		os.Exit(1)
+	}
+
+
+	
 	var app *fiber.App = fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -74,28 +84,52 @@ func main() {
 		LongURL string `json:"url" form:"url"`
 	}
 
+	type response struct {
+		LongURL string `json:"long_url"`
+		ShortURL string `json:"short_url"`
+	}
+
 	//DB : urlshortenerdb TABLE: urlmapping
-	//TODO: Implement the shorten API which takes in the long URL in JSON Body of the request and returns the short URL
 	app.Post("/shorten/", func(c *fiber.Ctx) error {
-		// counter++
-		// return c.SendString(convertToBase62(counter))
 		url := new(URL)
 		if err := c.BodyParser(url); err != nil {
 			fmt.Println(err);
             return c.SendString("Unable to process request!")
         }
 		fmt.Println(url.LongURL);
-		return c.SendString("You want to shorten: " + url.LongURL)
+
+		//Ignoring domain, only inserting the unique part of shorturl inside db.
+		shortURLHash := convertToBase62(counter)
+		counter += 1
+		db.Exec("UPDATE tracker SET value=$1 WHERE name=$2",counter,"counter")
+		insertSQLStatement:= `INSERT INTO urlmapping(long_url, short_url, created_on) VALUES ($1, $2, $3)`
+		_, err := db.Exec(insertSQLStatement, url.LongURL, shortURLHash, time.Now())
+		if err!= nil {
+			panic(err)
+		}
+		completeShortUrl := domainName + shortURLHash
+		reponseObj := response{
+			LongURL: url.LongURL,
+			ShortURL: completeShortUrl,
+		}
+		return c.JSON(reponseObj)
 	})
 
-	//FIXME: Persist the shortedned URLs into DB, and fetch the corresponding long URLs from DB when user requests for it.
-	// app.Get("/:shortenedurl/", func(c *fiber.Ctx) error {
-	// 	fmt.Println(c.Params("shortenedurl"))
-	// 	if c.Params("shortenedurl") == "V0q" {
-	// 		return c.Redirect("https://www.google.co.in")
-	// 	}
-	// 	return c.Redirect("http://localhost:3000/")
-	// })
+	app.Get("/:shortenedurl/", func(c *fiber.Ctx) error {
+		fmt.Println(c.Params("shortenedurl"))
+		sqlStatement := `SELECT long_url from urlmapping WHERE short_url = $1;`
+		var longURL string
+		row := db.QueryRow(sqlStatement,c.Params("shortenedurl"))
+		switch err := row.Scan(&longURL); err{
+		case sql.ErrNoRows:
+			//FIXME: Add 404 not found page at the error URL
+			return c.Redirect("http://localhost:3000/error")
+		case nil:
+			return c.Redirect(longURL)
+		default:
+			panic(err)
+		}
+	})
 
 	app.Listen(":3000")
 }
